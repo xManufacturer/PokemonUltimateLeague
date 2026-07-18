7c4cfed26809b411e1f2c34bc0ed018980bd00e7
Manuel <mbernal.1998@gmail.com>
2026-07-18 17:39:46 +0200

    Versión Beta

--- competiciones/kanto.php
+++ competiciones/kanto.php
@@ -24,9 +24,20 @@ $resultado = $stmt->get_result();
     <title>Liga Kanto</title>
     <link rel="icon" href="../img/icono.png" type="image/png">
     <link rel="stylesheet" href="../css/style.css">
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TZL2J8ZT');</script>
+<!-- End Google Tag Manager -->
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZL2J8ZT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <h1>Liga Kanto</h1>
     <a href="../index.php" class="btn-inicio">
         <img src="../img/inicio.png" alt="Inicio">
