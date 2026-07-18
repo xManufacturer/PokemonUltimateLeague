126b0a3e339b1a0706ed796f4b73248235e74f23
Manuel <mbernal.1998@gmail.com>
2026-07-18 15:52:47 +0200

    Versión Beta

--- index.php
+++ index.php
@@ -134,9 +134,20 @@ $fechaActualizacion = $fecha->format("d/m/Y");
     <title>Pokémon Ultimate League</title>
     <link rel="icon" href="img/icono.png" type="image/png">
     <link rel="stylesheet" href="css/style.css">
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TZL2J8ZT');</script>
+<!-- End Google Tag Manager -->
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZL2J8ZT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <header class="cabecera-principal">
         <div class="contenedor-iconos-sociales">
             <a class="enlace-icono" href="https://discord.gg/5CzWTMf8aq" target="_blank">
